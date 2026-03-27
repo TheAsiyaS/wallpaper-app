b766f04c6a9d21aaad253423ad22d60f5a19683a
TheAsiyaS <theasiya.s@gmail.com>
2026-03-27 21:13:32 +0530

    feat: initial splash screen

---
+++ lib/presentation/screens/splashScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+       
+
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container(
+        height: size.height,
+        width: size.width,
+        color: Colors.black, 
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text(
+                  'OPAQUE',
+                  style: TextStyle(
+                    fontSize: 48,
+                    fontWeight: FontWeight.w900,
+                    letterSpacing: 10,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Optional: a thin minimalist accent line
+              Container(
+                height: 1,
+                width: 60,
+                color: Colors.white38,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
